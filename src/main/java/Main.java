fdfdcef7eef1e93a5b6b416c71300e3adece9f5f
Vimer969 <lapkovskyivan@yandex.ru>
2025-04-18 12:19:10 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 // Класс Автомобиль
 class Car {
-    String name;
-    int speed;
+    private String name;
+    private int speed;
 
     public Car(String name, int speed) {
         this.name = name;
@@ -17,19 +17,22 @@ class Car {
     public String getName() {
         return name;
     }
+
+    public int getSpeed() {
+        return speed;
+    }
 }
 
 // Класс Гонка
 class Race {
-    Car winner;
+    private Car winner;
 
-    public void determineWinner(Car car1, Car car2, Car car3) {
-        winner = car1;
-        if (car2.getDistance() > winner.getDistance()) {
-            winner = car2;
-        }
-        if (car3.getDistance() > winner.getDistance()) {
-            winner = car3;
+    public void determineWinner(Car[] cars) {
+        winner = cars[0];
+        for (int i = 1; i < cars.length; i++) {
+            if (cars[i].getDistance() > winner.getDistance()) {
+                winner = cars[i];
+            }
         }
     }
 
@@ -38,49 +41,59 @@ class Race {
     }
 }
 
-// Класс Main
+// Главный класс
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Car[] cars = new Car[3];
+        int numberOfCars = 3; // легко поменять
+        Car[] cars = new Car[numberOfCars];
 
-        for (int i = 0; i < 3; i++) {
-            String name;
-            int speed;  // убрали начальную инициализацию переменной
-
-            System.out.println("Введите название машины №" + (i + 1) + ":");
-            while (true) {
-                name = scanner.nextLine().trim();
-                if (!name.isEmpty()) break;
-                System.out.println("Название не может быть пустым. Введите ещё раз:");
-            }
-
-            System.out.println("Введите скорость машины №" + (i + 1) + ":");
-            while (true) {
-                String input = scanner.nextLine().trim();
-                if (isValidSpeed(input)) {
-                    speed = Integer.parseInt(input);
-                    if (speed > 0 && speed <= 250) {
-                        break;
-                    } else {
-                        System.out.println("Неправильная скорость. Введите число от 1 до 250:");
-                    }
-                } else {
-                    System.out.println("Неправильная скорость. Введите целое число:");
-                }
-            }
-
-            cars[i] = new Car(name, speed);
+        for (int i = 0; i < numberOfCars; i++) {
+            cars[i] = createCar(i + 1, scanner);
         }
 
         Race race = new Race();
-        race.determineWinner(cars[0], cars[1], cars[2]);
-        System.out.println("Самая быстрая машина: " + race.getWinner().getName());
+        race.determineWinner(cars);
+
+        Car winner = race.getWinner();
+        System.out.println("\n🏁 Победитель гонки: " + winner.getName());
+        System.out.println("Скорость: " + winner.getSpeed() + " км/ч");
+        System.out.println("Проехал за 24 часа: " + winner.getDistance() + " км");
+    }
+
+    // Метод создания машины с вводом и валидацией
+    private static Car createCar(int number, Scanner scanner) {
+        String name;
+        int speed;
+
+        System.out.println("Введите название машины №" + number + ":");
+        while (true) {
+            name = scanner.nextLine().trim();
+            if (!name.isEmpty()) break;
+            System.out.println("Название не может быть пустым. Введите ещё раз:");
+        }
+
+        System.out.println("Введите скорость машины №" + number + ":");
+        while (true) {
+            String input = scanner.nextLine().trim();
+            if (isValidSpeed(input)) {
+                speed = Integer.parseInt(input);
+                if (speed > 0 && speed <= 250) {
+                    break;
+                } else {
+                    System.out.println("Неправильная скорость. Введите число от 1 до 250:");
+                }
+            } else {
+                System.out.println("Неправильная скорость. Введите целое число:");
+            }
+        }
+
+        return new Car(name, speed);
     }
 
     // Метод для проверки, является ли строка числом
     private static boolean isValidSpeed(String input) {
-        // Проверка, что строка состоит только из цифр
+        if (input.isEmpty()) return false;
         for (int i = 0; i < input.length(); i++) {
             if (!Character.isDigit(input.charAt(i))) {
                 return false;
